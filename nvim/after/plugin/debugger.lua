69f02373588a84cb090d6392b50f29657c174e9b
rafael-almeida <rafael.almeida@example.com>
2023-02-12 11:59:49 -0500

    update nvim

--- nvim/after/plugin/debugger.lua
+++ nvim/after/plugin/debugger.lua
@@ -1,23 +1,43 @@
-vim.keymap.set("n", "<leader>bb", ":lua require('dap').toggle_breakpoint()<CR>")
+vim.keymap.set("n", "<leader>B", ":lua require('dap').toggle_breakpoint()<CR>")
+vim.keymap.set('n', '<Leader>dl', function()
+    require('dap').set_breakpoint(nil, nil, vim.fn.input('log message: '))
+end)
 
-vim.keymap.set("n", "<leader>bc", ":lua require('dap').continue()<CR>")
-vim.keymap.set("n", "<leader>bt", ":lua require('dap').terminate()<CR>")
+vim.keymap.set("n", "<leader>dc", ":lua require('dap').continue()<CR>")
+vim.keymap.set("n", "<leader>di", ":lua require('dap').step_into()<CR>")
+vim.keymap.set("n", "<leader>dv", ":lua require('dap').step_over()<CR>")
+vim.keymap.set("n", "<leader>do", ":lua require('dap').step_out()<CR>")
+vim.keymap.set("n", "<leader>dr", ":lua require('dap').restart()<CR>")
+vim.keymap.set("n", "<leader>dt", ":lua require('dap').terminate()<CR>")
 
-vim.keymap.set("n", "<leader>bo", ":lua require('dap').step_over()<CR>")
-vim.keymap.set("n", "<leader>bi", ":lua require('dap').step_into()<CR>")
-vim.keymap.set("n", "<leader>bO", ":lua require('dap').step_out()<CR>")
 
-vim.keymap.set("n", "<leader>bR", ":lua require('dap').repl.toggle()<CR>")
+-- ============================================================================
 
-local dap_ui = require("dapui")
-dap_ui.setup()
+local dap, dapui = require("dap"), require("dapui")
 
-vim.keymap.set("n", "<leader>bu", ":lua require('dapui').toggle()<CR>")
+dapui.setup()
+
+dap.listeners.after.event_initialized["dapui_config"] = function()
+    dapui.open()
+end
+
+dap.listeners.before.event_terminated["dapui_config"] = function()
+    dapui.close()
+end
+
+dap.listeners.before.event_exited["dapui_config"] = function()
+    dapui.close()
+end
+
+-- ============================================================================
 
 local dap_python = require("dap-python")
+
 dap_python.setup("~/.virtualenvs/debugpy/bin/python")
 -- dap_python.test_runner = 'pytest'
 
-vim.keymap.set("n", "<leader>bm", ":lua require('dap-python').test_method()<CR>")
-vim.keymap.set("n", "<leader>bC", ":lua require('dap-python').test_class()<CR>")
-vim.keymap.set("v", "<leader>bS", "<ESC>:lua require('dap-python').debug_selection()<CR>")
+vim.keymap.set("n", "<leader>dM", ":lua require('dap-python').test_method()<CR>")
+vim.keymap.set("n", "<leader>dC", ":lua require('dap-python').test_class()<CR>")
+vim.keymap.set("v", "<leader>dS", "<ESC>:lua require('dap-python').debug_selection()<CR>")
+
+-- ============================================================================
