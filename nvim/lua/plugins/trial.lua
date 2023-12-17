88520af946daf1a3985aa347bd3cc82bac86202b
rafael-almeida <rafael.almeida@example.com>
2023-12-17 01:54:44 -0500

    update

--- nvim/lua/plugins/trial.lua
+++ nvim/lua/plugins/trial.lua
@@ -1,3 +1,45 @@
 return {
-
+    {
+        "cshuaimin/ssr.nvim",
+        keys = {
+            {
+                "<leader>sR",
+                function()
+                    require("ssr").open()
+                end,
+                mode = { "n", "x" },
+                desc = "ssr: Start structural search and replace",
+            },
+        },
+    },
+    {
+        "simrat39/symbols-outline.nvim",
+        dependencies = { "neovim/nvim-lspconfig" },
+        cmd = "SymbolsOutline",
+        keys = {
+            { "<leader>cs", "<Cmd>SymbolsOutline<CR>", desc = "Symbols Outline" },
+        },
+        opts = {}
+    },
+    {
+        "smjonas/inc-rename.nvim",
+        cmd = "IncRename",
+        keys = {
+            { "<leader>ri", ":IncRename ", desc = "Incremental Rename" },
+        },
+        opts = {}
+    },
+    {
+        "folke/twilight.nvim",
+        cmd = "Twilight",
+        opts = {
+            dimming = {
+                alpha = 0.35,
+                inactive = true, -- Dims other windows
+            },
+        },
+        keys = {
+            { "<leader>z", "<Cmd>Twilight<CR>", desc = "twilight: Toggle Twilight" }
+        },
+    },
 }
