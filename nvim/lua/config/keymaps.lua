8be6e35d6438923ca09cc4702eaa0d3ef72d1ca9
rafael-almeida <rafael.almeida@example.com>
2025-01-18 19:30:30 -0500

    wip

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -10,7 +10,7 @@ util.noremap("n", "<Right>", "<nop>")
 util.noremap("n", "Q", "<nop>")
 
 -- Toggle Explorer
--- util.noremap("n", "<leader>n", ":Exp<cr>")
+util.noremap("n", "<leader>n", ":Exp<cr>")
 
 -- Ignore line break character when Yanking
 util.noremap("n", "Y", "yg$")
