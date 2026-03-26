08f2f18009d8c9e18250fc72f4483d2cd106bfbf
rafael-almeida <13210596+rafael-almeida@users.noreply.github.com>
2026-03-26 09:29:35 -0400

    migrate to neovim v0.11 lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -19,6 +19,7 @@ vim.g.maplocalleader = " "
 require("config.options")
 require("config.keymaps")
 require("config.autocmds")
+require("config.lsp")
 
 -- Load plugins
 require("lazy").setup("plugins", {
